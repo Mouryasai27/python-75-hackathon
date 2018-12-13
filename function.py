b00d6ef525a6e02f51d48650bbf346949277682e
MOURYA SAI CHARAN REDDY KYPA <36075316+Mouryasai27@users.noreply.github.com>
2018-12-13 21:33:48 +0530

    Delete function.py

--- function.py
+++
@@ -1,9 +0,0 @@
-
-#Factorial of a Number using Functions
-def Factorial(n): 
-    if n == 1:
-        return n
-    else:
-        return n*Factorial(n-1)
-num = int(input("enter the number:"))
-print(Factorial(num))
