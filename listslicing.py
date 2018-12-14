026c9b998af17498161f49767b6efd7d78135722
MOURYA SAI CHARAN REDDY KYPA <36075316+Mouryasai27@users.noreply.github.com>
2018-12-14 10:48:03 +0530

    Add files via upload

---
+++ listslicing.py
@@ -0,0 +1,9 @@
+#Slicing the Lists
+
+List = ['a','b','c','d','e','f']
+sli = List[0:4]
+print(sli)      #prints the List with first four elements
+print("\n")
+
+sli2 = List[1:4]
+print(sli2)     #prints only elements from 1st position to 3rd position
---
+++ stringslicing.py
@@ -0,0 +1,15 @@
+#String Slicing
+
+str = "I Love Programming"
+slice1 = str[0:1]
+print(slice1)       #output is 'I'
+
+slice2 = str[2:6]
+print(slice2)       #output is 'Love'           
+
+slice3 = str[7:18]
+print(slice3)       #output is 'programming'
+
+slice4 = str[17::-1]
+print(slice4)       #output will be reversing the String
+                    #output is 'gnimmargorP evoL I'
