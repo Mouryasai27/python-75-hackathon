fc274398b3534be468fe08d4b850fd0e54625ac9
MOURYA SAI CHARAN REDDY KYPA <36075316+Mouryasai27@users.noreply.github.com>
2018-12-14 17:28:32 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,10 @@
+#Loops in Python
+
+#sum of n numbers using for loop
+
+n = int(input("enter the number:"))
+sum = 0
+for i in range(1,n+1):
+     sum = sum + i
+print(sum)          #prints the sum of given n numbers
+                
---
+++ whileloop.py
@@ -0,0 +1,11 @@
+
+#while loop in Python
+
+n = int(input("Enter the required number:"))
+sum = 0
+i = 0
+while i<n+1:
+    sum = sum + i
+    i += 1
+print(sum)      #prints the sum of n numbers
+    
